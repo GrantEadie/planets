7aade96a88ea04ef6c17387bc9524a55f9c1f73f
Grant Eadie <grantleadie@gmail.com>
2020-09-04 11:37:58 -0700

    install html plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -6,6 +7,13 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Shape Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
   module: {
     rules: [
       {
