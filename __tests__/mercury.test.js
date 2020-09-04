d248875d0ed0b0b1579c98313886fe62795758df
Grant Eadie <grantleadie@gmail.com>
2020-09-04 14:13:48 -0700

    pass mercury functino test.

--- __tests__/mercury.test.js
+++ __tests__/mercury.test.js
@@ -5,10 +5,18 @@ describe('Mercury', () => {
   let mercury;
 
   beforeEach(() => {
-    mercury = new Mercury();
+    mercury = new Mercury(60);
   });
 
   test('check creation of venus object', () => {
-  expect(mercury).toMatchObject(new Mercury());
+  expect(mercury).toMatchObject(new Mercury(60));
+  });
+
+  test('check that values are entering Mercury class', () => {
+    expect(mercury.age).toEqual(60);
+  });
+
+  test('expect function outputAge to return 60 * .24', () => {
+    expect(mercury.outputAge()).toEqual(14);
   });
 });
--- src/mercury.js
+++ src/mercury.js
@@ -1,5 +1,10 @@
 export default class Mercury {
-  constructor() {
-    
+  constructor(age) {
+    this.age = age;
+    this.years = .24;
+  }
+
+  outputAge () {
+    return Math.round(this.age * this.years);
   }
 }
