dc6dbcc32165dc33b967f58c6f8bf0267d797b4b
Grant Eadie <grantleadie@gmail.com>
2020-09-07 17:26:48 -0700

    begin making UI.

--- src/main.js
+++ src/main.js
@@ -2,3 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Mars from './mars.js';
+import Mercury from './mercury.js';
+import Jupiter from './jupiter.js';
+import Venus from './venus.js';
+
+$(document).ready(function() {
+  let jupiter = new Jupiter();
+  let mars = new Mars();
+  let mercury = new Mercury();
+  let venus = new Venus();
+
+  $("planet-age-form").submit(function() {
+    let inputtedAge = $("#age").val();
+
+  });
+
+});
